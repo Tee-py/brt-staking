0c27ea3c29f6abb3117df153c702692340ddf2ff
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2022-03-16 23:05:22 +0100

    updated tests

--- test/test.js
+++ test/test.js
@@ -14,9 +14,32 @@ describe("Test", function () {
     await staking.deployed();
     
     //Approve Staking Contract To Spend From Reserve
-    brt.approve(staking, brt.balanaceOf(owner));
+    const approveTx = await brt.approve(staking, brt.balanaceOf(owner));
+    const appRect = await approveTx.wait();
+
+    //Send Some Tokens to addr1 for Staking
+    const transTx = await brt.transfer(addr1.address, 10000);
+    const transRecp = await transTx.wait();
+
+    //Approve Staking Contract To Spend BRT Token from addr1
+    const appTrans = await brt.connect(addr1).approve(staking.address, 10000);
+    const appRecp = await appTrans.wait();
+
+    //Stake BRT Token
+    const stakeTx = await staking.connect(addr1).stake(10000)
+    const stakeRecpt = await stakeTx.wait()
+
+    //Get Staking Balance
+    const stakeBalance = await staking.connect(addr1).getBalance(addr1.address);
+    const balRecpt = await stakeBalance.wait();
+
 
     console.log(`BRT Token deployed to: ${brt.address}`);
     console.log(`Staking Contract Deployed to: ${staking.address}`);
+    console.log(appRect);
+    console.log(appRecp);
+    console.log(transRecp);
+    console.log(stakeRecpt);
+    console.log(balRecpt);
   });
 });
