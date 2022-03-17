5905fe724bafaa584731bc17179db811bba01ebe
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2022-03-17 17:47:55 +0100

    Added compounding to stake

--- contracts/BRTStaking.sol
+++ contracts/BRTStaking.sol
@@ -17,6 +17,7 @@ contract BRTStaking {
 
     mapping(address => uint) balances;
     mapping(address => uint) stakedAt;
+    mapping(address => uint) lastClaimed;
     mapping(address => uint) nextReward;
 
     event Stake(address indexed _owner, uint _amount);
@@ -57,12 +58,25 @@ contract BRTStaking {
     }
 
     function stake(uint _amount) external hasNFT hasSufficientAllowance(_amount) notReserve {
+        //Checks If User Has Current Stake
+        if (balances[msg.sender] > 0){
+            //check if stake is already more that 3 days and calculate reward
+            if (stakedAt[msg.sender] > 3 days){
+                //calculate and add to staker balance
+                uint curr_reward = (block.timestamp - lastClaimed[msg.sender])*balances[msg.sender]*10/2592000;
+                balances[msg.sender] += curr_reward;
+                totalStaked += curr_reward;
+                lastClaimed[msg.sender] = block.timestamp;
+                emit Claim(msg.sender, curr_reward);
+            }
+        }
         bool res = brtToken.transferFrom(msg.sender, tokenReserve, _amount);
         require(res == true, "Failed");
         balances[msg.sender] += _amount;
         totalStaked += _amount;
-        stakedAt[msg.sender] = block.timestamp;
-        nextReward[msg.sender] = block.timestamp + 30 days;
+        if (stakedAt[msg.sender] == 0){
+            stakedAt[msg.sender] = block.timestamp;
+        }
         emit Stake(msg.sender, _amount);
     }
 
@@ -70,23 +84,22 @@ contract BRTStaking {
         return balances[_addr];
     }
 
-    function claim() external canClaim {
+    /*function claim() external canClaim {
         uint reward = balances[msg.sender]/10;
         bool res = brtToken.transferFrom(tokenReserve, msg.sender, reward);
         require(res == true, "Failed");
         nextReward[msg.sender] = block.timestamp + 30 days;
         emit Claim(msg.sender, reward);
-    }
+    }*/
 
     function withdraw() external hasStake {
         bool res;
         uint balance = balances[msg.sender];
-        if (nextReward[msg.sender] >= block.timestamp) {
-            uint reward = balances[msg.sender]/10;
-            res = brtToken.transferFrom(tokenReserve, msg.sender, reward);
-            require(res == true, "Error");
-            emit Claim(msg.sender, reward);
+        uint curr_reward = 0;
+        if (block.timestamp - stakedAt[msg.sender] >= 3 days){
+            curr_reward = (block.timestamp - lastClaimed[msg.sender])*balances[msg.sender]*10/2592000;
         }
+        balance += curr_reward;
         res = brtToken.transferFrom(tokenReserve, msg.sender, balance);
         balances[msg.sender] = 0;
         require(res == true, "Error");
